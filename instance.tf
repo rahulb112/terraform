ec1808a85fa41bf3ede4ef45896b32d37a653928
rahulb112 <rahul112cbe@gmail.com>
2021-06-08 16:24:41 +0000

    new files

---
+++ instance.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "my_ec2" {
+ami = "${var.ami}"
+instance_type = "t2.micro"
+subnet_id = "${aws_subnet.my_subnet1.id}"
+user_data = "${file("path/file_name")}"
+vpc_security_group_ids = ["${aws_security_group.my_sg1.id}"]
+tags = {
+        Name = "my-ec2"
+}
+}
+
--- vpc.tf
+++ vpc.tf
@@ -15,16 +15,6 @@ Name = "SN1"
 }
 }
 
-resource "aws_instance" "my_ec2" {
-ami = "${var.ami}"
-instance_type = "t2.micro"
-subnet_id = "${aws_subnet.my_subnet1.id}"
-user_data = "${file("path/file_name")}"
-vpc_security_group_ids = ["${aws_security_group.my_sg1.id}"]
-tags = {
-	Name = "my-ec2"
-}
-}
 
 resource "aws_security_group" "my_sg1" {
 vpc_id = "${aws_vpc.my_vpc.id}"
