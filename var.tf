2e6e5cbe2d2706e48c2457d65e959b578338b131
rahulb112 <85565311+rahulb112@users.noreply.github.com>
2021-06-10 15:47:09 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -2,20 +2,14 @@ variable "vpc_cidr" {
 default = "10.1.0.0/24"
 }
 
-variable "pub1_subnet_cidr" {
-default = "10.1.0.0/26"
+variable "pub_subnet_cidr" {
+type = "List"
+default = ["10.1.0.0/26","10.1.0.64/26"]
 }
 
-variable "pub2_subnet_cidr" {
-default = "10.1.0.64/26"
-}
-
-variable "prv1_subnet_cidr" {
-default = "10.1.0.128/26"
-}
-
-variable "prv2_subnet_cidr" {
-default = "10.1.0.192/26"
+variable "prv_subnet_cidr" {
+type = "List"
+default = ["10.1.0.128/26","10.1.0.192/26"]
 }
 
 variable "ami" {
