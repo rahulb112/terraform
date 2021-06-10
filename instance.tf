88a3af4b089be23e6b5b5dd29c3a9fbd3156142b
rahulb112 <85565311+rahulb112@users.noreply.github.com>
2021-06-10 19:36:08 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -3,9 +3,14 @@ ami = "${var.ami}"
 instance_type = "t2.micro"
 subnet_id = "${aws_subnet.my_subnet1.id}"
 user_data = "${file("path/apache.sh")}"
+key_name = "{aws_key_pair.ec2-key.key_name}"
 vpc_security_group_ids = ["${aws_security_group.my_sg1.id}"]
 tags = {
         Name = "my-ec2"
 }
 }
 
+resource "aws_key_pair" "ec2-key" {
+  key_name   = "key.pub"
+  public_key = "ssh-rsa XXXXXXX email@example.com"
+}
