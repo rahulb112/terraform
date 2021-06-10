c2374337840b89a3ef0320e4ec4af2d746ed5caf
rahulb112 <85565311+rahulb112@users.noreply.github.com>
2021-06-10 21:15:34 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,18 +1,19 @@
 resource "aws_instance" "my_ec2" {
-ami = "${var.ami}"
-instance_type = "t2.micro"
-subnet_id = "${aws_subnet.my_subnet1.id}"
-user_data = "${file("path/apache.sh")}"
-key_name = "{aws_key_pair.ec2-key.key_name}"
-vpc_security_group_ids = ["${aws_security_group.web_sg1.id}"]
-ebs_block_device = [
+  ami = "${var.ami}"
+  instance_type = "t2.micro"
+  subnet_id = "${aws_subnet.my_subnet1.id}"
+  user_data = "${file("apache.sh")}"
+  key_name = "${aws_key_pair.ec2-key.key_name}"
+  iam_instance_profile = "${aws_iam_instance_profile.ec2_profile.name}"
+  vpc_security_group_ids = ["${aws_security_group.web_sg1.id}"]
+  ebs_block_device = [
     {
       device_name = "/dev/sdf"
       volume_type = "gp2"
       volume_size = 8
     }
   ]
-tags = {
+  tags = {
         Name = "my-ec2"
 }
 }
@@ -23,26 +24,26 @@ resource "aws_key_pair" "ec2-key" {
 }
 
 resource "aws_security_group" "web_sg1" {
-vpc_id = "${aws_vpc.my_vpc.id}"
+  vpc_id = "${aws_vpc.my_vpc.id}"
 	
-egress {
-	from_port = 0
-	to_port = 0
-	protocol = "-1"
-	cidr_blocks = ["0.0.0.0/0"]
-	}
+  egress {
+	  from_port = 0
+	  to_port = 0
+	  protocol = "-1"
+	  cidr_blocks = ["0.0.0.0/0"]
+	  }
 	
-ingress {
-	from_port = 22
-	to_port = 22
-	protocol = "tcp"
-	cidr_blocks = ["10.1.0.0/24"]
-	}
+  ingress {
+	  from_port = 22
+	  to_port = 22
+	  protocol = "tcp"
+	  cidr_blocks = ["10.1.0.0/24"]
+	  }
 	
-ingress {
-	from_port = 80
-	to_port = 80
-	protocol = "tcp"
-	cidr_blocks = ["0.0.0.0/0"]
-	}	
+  ingress {
+	  from_port = 80
+	  to_port = 80
+	  protocol = "tcp"
+	  cidr_blocks = ["0.0.0.0/0"]
+	  }	
 }
