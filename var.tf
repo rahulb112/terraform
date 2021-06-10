687454860fbcec07d233367438e955ba3ba2cd3c
rahulb112 <85565311+rahulb112@users.noreply.github.com>
2021-06-10 15:56:08 +0530

    count added

--- var.tf
+++ var.tf
@@ -12,6 +12,11 @@ type = "List"
 default = ["10.1.0.128/26","10.1.0.192/26"]
 }
 
+variable "subnet_az" {
+type = "List"
+default = ["eu-west-2a","eu-west-2b"]
+}
+
 variable "ami" {
 default = "ami-xxxxxxxx"
 }
