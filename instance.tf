321f842e8ee58e0ddb5b18eb7b603a94df0c8bee
rahulb112 <85565311+rahulb112@users.noreply.github.com>
2021-06-11 17:47:22 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -36,10 +36,10 @@ resource "aws_security_group" "web_sg1" {
   dynamic "ingress" {
     for_each = var.sg_ports
     content {
-    from_port   = 22
-    to_port     = 22
+    from_port   = ingress.value
+    to_port     = ingress.value
     protocol    = "tcp"
-    cidr_blocks = ["10.1.0.0/24"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   }
 }
