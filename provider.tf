7b090435c1e590a977e544fbacbf3b2138857751
rahulb112 <85565311+rahulb112@users.noreply.github.com>
2021-06-11 19:05:26 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "test-sagemaker-bucket3"
+    bucket = "test-sagemaker-bucket4"
     key    = "terraform.tfstate"
     region = "eu-west-2"
   }
