d8dc8ad8c1497840b258f80289705a79462055cd
rahulb112 <85565311+rahulb112@users.noreply.github.com>
2021-06-11 17:39:21 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -42,11 +42,5 @@ resource "aws_security_group" "web_sg1" {
     cidr_blocks = ["10.1.0.0/24"]
   }
   }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
+
