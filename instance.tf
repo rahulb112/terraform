2f06ffa6a8bdc99cdbe9af1573a863e84350b354
rahulb112 <85565311+rahulb112@users.noreply.github.com>
2021-06-11 20:22:39 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -6,13 +6,13 @@ resource "aws_instance" "my_ec2" {
   key_name               = aws_key_pair.ec2-key.key_name
   iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
   vpc_security_group_ids = ["${aws_security_group.web_sg1.id}"]
-  ebs_block_device = [
+  ebs_block_device
     {
       device_name = "/dev/sdf"
       volume_type = "gp2"
       volume_size = 8
     }
-  ]
+  
   tags = {
     Name = "my-ec2"
   }
